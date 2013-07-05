a430b0ef674194b91d0abb18b4b9d425a0217ece
Javad Karabi <jkarabi@caxy.com>
2013-07-05 17:59:50 -0500

    Create concert model.
    lint

--- js/app.js
+++ js/app.js
@@ -1,13 +1,16 @@
-    Backbone.Tastypie.apiKey.username="guest";
-    Backbone.Tastypie.apiKey.key="6b0ed8aef817c6002850b6fa301915c2485ee8eb";
+Backbone.Tastypie.apiKey.username = "guest";
+Backbone.Tastypie.apiKey.key = "6b0ed8aef817c6002850b6fa301915c2485ee8eb";
 
-    var api_url = "http://boricide"
+var api_url = "http://boricide";
 
-    var ConcertCollection = Backbone.Collection.extend({
-        url: function() {
-            return api_url + "/api/v1/concert/?format=json";
-        },
-    });
+var ConcertModel = Backbone.Model.extend({
+});
 
-    var concerts = new ConcertCollection();
-    concerts.fetch();
+var ConcertCollection = Backbone.Collection.extend({
+  url: function () {
+    return api_url + "/api/v1/concert/";
+  },
+  model: ConcertModel
+});
+
+var concerts = new ConcertCollection();

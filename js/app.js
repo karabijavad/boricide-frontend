a576a0a4a2a106cc1d3934071d3d8c2a96f87c83
Javad Karabi <jkarabi@caxy.com>
2013-07-05 22:22:50 -0500

    Create Venue model/collection.

--- js/app.js
+++ js/app.js
@@ -3,6 +3,14 @@ Backbone.Tastypie.apiKey.key = "6b0ed8aef817c6002850b6fa301915c2485ee8eb";
 
 var api_url = "http://boricide";
 
+var VenueModel = Backbone.Model.extend({
+});
+
+var VenueCollection = Backbone.Collection.extend({
+  model: VenueModel
+});
+
+
 var ConcertModel = Backbone.Model.extend({
 });
 
